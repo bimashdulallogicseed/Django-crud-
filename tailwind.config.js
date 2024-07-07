a8562cd7ec71c2aeabe672421d327430d08fda56
Bimash Dulal <bimash.dulal.logicseed@gmail.com>
2024-07-07 13:57:13 +0545

    after profiie update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './crud/templates/**/*.html'
   ],
   theme: {
-    extend: {},
+      extend: {
+        animation: {
+          marquee: 'marquee 10s linear infinite',
+          marquee2: 'marquee 10s linear infinite',
+        },
+        keyframes: {
+          marquee: {
+            '0%': { transform: 'translateX(0%)' },
+            '100%': { transform: 'translateX(-100%)' },
+          },
+           marquee2: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0%)' },
+      },
+        },
+    
+    },
   },
   plugins: [],
 }
